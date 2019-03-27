e3198f7fb0dcd30e6a3091ad07c98f22e1016240
squarefaceyao <squarefaceyao@gmail.com>
2019-03-27 09:39:12 +0800

    modify logging

---
+++ src/test/java/com/imooc/sell/LoggerTest.java
@@ -0,0 +1,35 @@
+package com.imooc.sell;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @Author squareface
+ * @Description //测试日志
+ * @Date 17:29 2019-03-26
+ * @Param
+ * @return 
+ **/
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class LoggerTest {
+
+
+
+    @Test
+    public void test1(){
+
+        log.debug("debug.....");
+        log.info("info.....");
+        log.error("error....");
+
+
+    }
+}
